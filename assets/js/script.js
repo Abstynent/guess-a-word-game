86481e3c9e4fd2508664cb9e69ac7e99ecf47492
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-18 13:54:09 +0000

    comments added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Declaration of all DOM elements
 var winsEl = document.querySelector("#wins");
 var lossesEl = document.querySelector("#losses");
 var timerEl = document.querySelector("#timer");
@@ -6,17 +7,17 @@ var btnStartEl = document.querySelector("#btn-start");
 var btnResetEl = document.querySelector("#btn-reset");
 var gameStatusEl = document.querySelector("#game-status");
 
+// Declarations of all needed variables
 var wins = 0;
 var losses = 0;
 var timer = 10;
 var originalWord = "";
 var letterGuessed = "";
 var blankWord = "";
-
+// Declaration of string containing all words used in the game
 var wordsBank = ["javascript", "boolean", "function", "argument", "document"];
 
-renderLastResults();
-
+// Declare function to update game statistics from localStorage
 function renderLastResults() {
     var savedWins = localStorage.getItem("wins");
     var savedLosses = localStorage.getItem("losses");
@@ -25,22 +26,24 @@ function renderLastResults() {
     renderGameStatistics();
 };
 
+// Declare function to render last statistics on the page
 function renderGameStatistics() {
     winsEl.innerHTML = wins;
     lossesEl.innerHTML = losses;
     timerEl.innerHTML = timer;
 };
 
-function endOfGame() {
-    gameEl.textContent = originalWord;
-    btnStartEl.disabled = false;
-    btnResetEl.disabled = false;
-    renderGameStatistics();
-    localStorage.setItem("wins",wins);
-    localStorage.setItem("losses",losses);
+// Declare function to prepare string of "_" that will be displayed on the page
+function setWord() {
     blankWord = "";
+    originalWord = wordsBank[Math.floor(Math.random() * wordsBank.length)];
+    for(var i = 0; i !== originalWord.length; i++) blankWord += "_";
+    gameEl.textContent = blankWord;
+    return blankWord;
 };
 
+// Declare function that after pressed key will check if user got a match, 
+// if yes adjust string and display on the page
 function renderBlankWord() {
     for(i = 0; i < originalWord.length; i++) {
         if(originalWord[i] === letterGuessed) {
@@ -50,18 +53,23 @@ function renderBlankWord() {
     };
 };
 
+// Declare function to adjust temporary string with letter found by user
 function setCharAt(str, index, chr) {
     if(index > str.length -1 ) return str;
     return str.substring(0,index) + chr + str.substring(index+1);
 };
 
-function setWord() {
-    originalWord = wordsBank[Math.floor(Math.random() * wordsBank.length)];
-    for(var i = 0; i !== originalWord.length; i++) blankWord += "_";
-    gameEl.textContent = blankWord;
-    return blankWord;
+// Declare function to display guessed word, enable buttons again and save game statistics.
+function endOfGame() {
+    gameEl.textContent = originalWord;
+    btnStartEl.disabled = false;
+    btnResetEl.disabled = false;
+    renderGameStatistics();
+    localStorage.setItem("wins",wins);
+    localStorage.setItem("losses",losses);
 };
 
+// Reset button, set localStorage data to 0, render to the page
 btnResetEl.addEventListener("click", function() {
     wins = 0;
     losses = 0;
@@ -70,21 +78,30 @@ btnResetEl.addEventListener("click", function() {
     renderGameStatistics();
 });
 // MAIN GAME CODE 
+
+// Display last results
+renderLastResults();
+
+ // Listen to pressed keys, check if are letter
 document.addEventListener("keydown", function(event) {
     if(timer === 0) return;
 
     var key = event.key.toLowerCase();
     var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
     if (alphabetNumericCharacters.includes(key)) letterGuessed = key;
-    renderBlankWord();
+    renderBlankWord(); // If letter is in original word, display and adjust string
 });
 
+// Start button
 btnStartEl.addEventListener("click", function() {
+    // Disable all buttons during the game
     btnStartEl.disabled = true;
     btnResetEl.disabled = true;
     var timeLeft = timer;
 
     blankWord = setWord();
+
+    // Start timer, check if user guessed the word or time is up
     var timeInterval = setInterval(function() {
         timeLeft--;
         timerEl.textContent = timeLeft;
